3df1e1103ccaacdf0d99eb193117ffda20581935
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2026-03-14 17:53:11 +0530

    Update internal URLs

--- server.ts
+++ server.ts
@@ -2231,7 +2231,7 @@ print("Enter a number:")
       };
       sprite = new Image();
       sprite.src =
-        "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+        "https://i.imgur.com/PfRWr3X.png" +
         "?" +
         new Date().getTime();
       sprite.setAttribute("crossOrigin", " ");
@@ -2243,7 +2243,7 @@ print("Enter a number:")
       };
     
       finishSprite = new Image();
-      finishSprite.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png"+
+      finishSprite.src = "https://i.imgur.com/iQ7mU25.png"+
       "?" +
       new Date().getTime();
       finishSprite.setAttribute("crossOrigin", " ");
